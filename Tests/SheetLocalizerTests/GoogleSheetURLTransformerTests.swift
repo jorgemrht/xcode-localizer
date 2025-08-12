2d50ee5df27457df1b8bbd7700fa8ce5d65789c4
jorgemrht <git@jorgemrht.dev>
2025-08-12 11:28:24 +0200

    feat: updated GoogleSheetURLTransformerTests

--- Tests/SheetLocalizerTests/GoogleSheetURLTransformerTests.swift
+++ Tests/SheetLocalizerTests/GoogleSheetURLTransformerTests.swift
@@ -4,40 +4,40 @@ import Testing
 @Suite()
 struct GoogleSheetURLTransformerTests {
 
-    @Test("Transform standard URL to CSV")
+    @Test
     func test_transform_standardURL() throws {
         let url = "https://docs.google.com/spreadsheets/d/12345"
         let expected = "https://docs.google.com/spreadsheets/d/12345/pub?output=csv"
         #expect(try GoogleSheetURLTransformer.transformToCSV(url) == expected)
     }
 
-    @Test("Transform URL with trailing slash to CSV")
+    @Test
     func test_transform_trailingSlashURL() throws {
         let url = "https://docs.google.com/spreadsheets/d/12345/"
         let expected = "https://docs.google.com/spreadsheets/d/12345/pub?output=csv"
         #expect(try GoogleSheetURLTransformer.transformToCSV(url) == expected)
     }
 
-    @Test("Transform /pubhtml URL to CSV")
+    @Test
     func test_transform_pubhtmlURL() throws {
-        let url = "https://docs.google.com/spreadsheets/d/e/some-long-id/pubhtml"
-        let expected = "https://docs.google.com/spreadsheets/d/e/some-long-id/pub?output=csv"
+        let url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRj3aWiQffPzhrWzu1E8B14_3YiqQ_uwh3K0yVANXJuUpCS3fJ523VIu3VDNyRnXQ5jCWeN3AL9XX0a/pubhtml"
+        let expected = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRj3aWiQffPzhrWzu1E8B14_3YiqQ_uwh3K0yVANXJuUpCS3fJ523VIu3VDNyRnXQ5jCWeN3AL9XX0a/pub?output=csv"
         #expect(try GoogleSheetURLTransformer.transformToCSV(url) == expected)
     }
     
-    @Test("Do not transform already correct CSV URL")
+    @Test
     func test_transform_alreadyCSVURL() throws {
-        let url = "https://docs.google.com/spreadsheets/d/e/some-long-id/pub?output=csv"
+        let url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRj3aWiQffPzhrWzu1E8B14_3YiqQ_uwh3K0yVANXJuUpCS3fJ523VIu3VDNyRnXQ5jCWeN3AL9XX0a/pub?output=csv"
         #expect(try GoogleSheetURLTransformer.transformToCSV(url) == url)
     }
     
-    @Test("Do not transform already correct export format URL")
+    @Test
     func test_transform_alreadyExportURL() throws {
         let url = "https://docs.google.com/spreadsheets/d/12345/export?format=csv"
         #expect(try GoogleSheetURLTransformer.transformToCSV(url) == url)
     }
 
-    @Test("Throws error for invalid or non-Google Sheets URLs")
+    @Test
     func test_transform_invalidInput() {
         #expect(throws: (any Error).self) {
             _ = try GoogleSheetURLTransformer.transformToCSV("")
